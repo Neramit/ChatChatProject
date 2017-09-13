154b9df0acbc74175c526df9b2a1b34246ceb19c
Neramit777 <godlovemit777@gmail.com>
2017-09-13 17:50:08 +0700

    Test Git #1

--- app/src/main/java/com/example/chatchatapplication/ChangeDisplayname.java
+++ app/src/main/java/com/example/chatchatapplication/ChangeDisplayname.java
@@ -1,26 +1,45 @@
 package com.example.chatchatapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dd.CircularProgressButton;
+import com.example.chatchatapplication.Object_json.User;
+import com.example.chatchatapplication.Object_json.registerSend;
+import com.example.chatchatapplication.Object_json.searchRetrieve;
+import com.google.gson.Gson;
 
-public class ChangeDisplayname extends AppCompatActivity {
+public class ChangeDisplayname extends AppCompatActivity implements jsonBack {
 
     SearchView displayName;
-    String displayname;
+    String displayname, token, target;
     CircularProgressButton button;
+    TextView count;
+
+    // Shared preferrence
+    SharedPreferences sp;
+    SharedPreferences.Editor mEdit1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle(R.string.title_profile);
         setContentView(R.layout.activity_change_displayname);
+
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        mEdit1 = sp.edit();
 
         displayName = (SearchView) findViewById(R.id.edittextDisplayname);
         button = (CircularProgressButton) findViewById(R.id.ok_button);
+        count = (TextView) findViewById(R.id.countDisplayname);
 
         button.setProgress(0);
         button.setIndeterminateProgressMode(true);
@@ -37,11 +56,62 @@ public class ChangeDisplayname extends AppCompatActivity {
             }
         });
 
-        // Remove icon search
-        int magId = getResources().getIdentifier("android:id/search_mag_icon", null, null);
-        ImageView magImage = (ImageView) displayName.findViewById(magId);
-        magImage.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
-        magImage.setVisibility(View.GONE);
+        displayName.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String s) {
+                count.setText(s.length() + "/20");
+                return false;
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayname = displayName.getQuery().toString();
+                Gson sendJson = new Gson();
+                button.setProgress(50);
+                User data = new User();
+                data.setDisplayName(displayname);
+                token = sp.getString("token", null);
+                registerSend send = new registerSend("Other", "profileAccountDisplayName", token, data);
+                String sendJson2 = sendJson.toJson(send);
+                new SimpleHttpTask(ChangeDisplayname.this).execute(sendJson2);
+            }
+        });
+    }
+
+    @Override
+    public void processFinish(String output) {
+        Gson gson = new Gson();
+        final searchRetrieve data = gson.fromJson(output, searchRetrieve.class);
+
+        if (data.getStatus() == 200) {
+            button.setProgress(100);
+            Toast.makeText(this, data.getMessage(), Toast.LENGTH_SHORT).show();
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(ChangeDisplayname.this, ProfileAccount.class));
+                    finish();
+                }
+            }, 700);
+
+        } else {
+            button.setProgress(-1);
+            Toast.makeText(this, data.getMessage(), Toast.LENGTH_SHORT).show();
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    button.setProgress(0);
+                }
+            }, 700);
+        }
     }
 }
--- app/src/main/java/com/example/chatchatapplication/friendFragment.java
+++ app/src/main/java/com/example/chatchatapplication/friendFragment.java
@@ -7,17 +7,75 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.example.chatchatapplication.Adapter.friendAdapter;
+
 public class friendFragment extends Fragment {
 
     ListView friendList;
+    private friendAdapter mAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mAdapter = new friendAdapter(getActivity());
         View view = null;
         view = inflater.inflate(R.layout.fragment_friend, container, false);
         friendList = (ListView) view.findViewById(R.id.friendListview);
-        
+
+        friendList.setAdapter(mAdapter);
+
+//        friendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // TODO Auto-generated method stub
+//
+//                view.setBackgroundColor(Color.parseColor("#C5F9D4"));
+//
+//                Intent intent = null;
+//                switch (((HospitalNames)list.getAdapter().getItem(position)).getHospitalName()) {
+////                switch (hospitalNameList[position])
+//                    case "โรงพยาบาลราษฎร์บูรณะ":
+//                        intent = new Intent(Find.this, Hospital_1.class);
+//                        break;
+//                    case "โรงพยาบาลสุขสวัสดิ์":
+//                        intent = new Intent(Find.this, Hospital_2.class);
+//                        break;
+//                    case "โรงพยาบาลประชาพัฒน์":
+//                        intent = new Intent(Find.this, Hospital_3.class);
+//                        break;
+//                    case "โรงพยาบาลบางปะกอก":
+//                        intent = new Intent(Find.this, Hospital_4.class);
+//                        break;
+//                    case "โรงพยาบาลบางปะกอก 3":
+//                        intent = new Intent(Find.this, Hospital_6.class);
+//                        break;
+//                    case "โรงพยาบาลบางปะกอก 9":
+//                        intent = new Intent(Find.this, Hospital_7.class);
+//                        break;
+//                    case "โรงพยาบาลเจริญกรุง":
+//                        intent = new Intent(Find.this, Hospital_5.class);
+//                        break;
+//                    case "โรงพยาบาลสมิติเวช":
+//                        intent = new Intent(Find.this, Hospital_8.class);
+//                        break;
+//                    case "โรงพยาบาลบํารุงราษฎร์":
+//                        intent = new Intent(Find.this, Hospital_9.class);
+//                        break;
+//                    case "โรงพยาบาลศิริราช":
+//                        intent = new Intent(Find.this, Hospital_10.class);
+//                        break;
+//                    default:
+//                        intent = new Intent(Find.this, Hospital_11.class);
+//                        break;
+//                }
+//                if (intent != null) {
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.enter, R.anim.exit);
+//                    finish();
+//                }
+//            }
+//        });
+
         return view;
     }
-
 }
